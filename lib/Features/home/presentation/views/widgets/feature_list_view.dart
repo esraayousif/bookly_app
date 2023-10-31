61f92fe99d8a48740dad57454f6e81505f8d0d89
codetime30 <ey989063@gmail.com>
2023-10-31 13:10:13 +0200

    Create Style File and add Title to Home View

---
+++ lib/Features/home/presentation/views/widgets/feature_list_view.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+import 'custom_listview_item.dart';
+
+class FeatherBooksListView extends StatelessWidget {
+  const FeatherBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 8),
+              child: FeatureListViewItem(),
+            );
+          }),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,38 +1,28 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_appbar.dart';
-import 'custom_listview_item.dart';
+import 'feature_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeatherBooksListView(),
-      ],
-    );
-  }
-}
-
-class FeatherBooksListView extends StatelessWidget {
-  const FeatherBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-        return const Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 8),
-          child: FeatureListViewItem(),
-        );
-      }),
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 24),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatherBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text('Best Seller', style: Styles.titleMedium,)
+        ],
+      ),
     );
   }
 }
