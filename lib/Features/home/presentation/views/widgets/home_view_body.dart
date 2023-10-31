fd99c6636c7e1a2bd19f173b98fe76a036dd6cbe
codetime30 <ey989063@gmail.com>
2023-10-31 17:06:32 +0200

    Add Styles to App

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets.dart';
 import 'custom_appbar.dart';
 import 'feature_list_view.dart';
 
@@ -20,7 +21,45 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          Text('Best Seller', style: Styles.titleMedium,)
+          Text('Best Seller', style: Styles.textStyle20,),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          height: 100,
+          width: 50,
+
+          decoration:  BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.red,
+              image:const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AssetsData.testImage),
+              )),
+        ),
+      ),
+          Column(
+            children: [
+
+            ],
+          )
+
         ],
       ),
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const KPrimaryColor =  Color(0xff100B20);
 const KTranstionDuration =  Duration(milliseconds: 250 );
+const KGtSectraFine = 'GT Sectra Fine';
