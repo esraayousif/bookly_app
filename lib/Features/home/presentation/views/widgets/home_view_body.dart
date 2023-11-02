07fb8788fa7c2a0c9c034150557d703bf8e5edda
codetime30 <ey989063@gmail.com>
2023-11-02 13:08:31 +0200

    Add Best Seller List View

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'best_seller_list_view_item.dart';
 import 'custom_appbar.dart';
 import 'feature_list_view.dart';
@@ -11,26 +11,47 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeatherBooksListView(),
-          SizedBox(
-            height: 50,
+
+    return  CustomScrollView(
+
+      physics:  BouncingScrollPhysics(),
+      slivers: [
+        const SliverToBoxAdapter(
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: KPaddingHorizontal30,
+                child: CustomAppBar(),
+              ),
+              FeatherBooksListView(),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: KPaddingHorizontal30,
+                child: Text(
+                  'Best Seller',
+                  style: Styles.textStyle20,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+            ],
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle20,
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+              List.generate(20, (index) => const Padding(
+                padding:  EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                child:  BestSellerListViewItem(),
+              ))
           ),
-          SizedBox(
-            height: 20,
-          ),
-          BestSellerListViewItem(),
-        ],
-      ),
+        )
+      ],
     );
+
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const KPrimaryColor =  Color(0xff100B20);
 const KTranstionDuration =  Duration(milliseconds: 250 );
 const KGtSectraFine = 'GT Sectra Fine';
+const KPaddingHorizontal30 = EdgeInsets.symmetric(horizontal: 30);
