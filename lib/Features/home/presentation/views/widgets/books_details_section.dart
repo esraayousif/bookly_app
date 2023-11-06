9985e0f4d5e85d7231de398f567122ca08b12628
codetime30 <ey989063@gmail.com>
2023-11-06 16:19:57 +0200

    Refactor Book Details View

---
+++ lib/Features/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
+import 'books_action.dart';
+import 'custom_book_image.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .17),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(
+          'The Jungle Book',
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+                fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const BooksAction(),
+      ],
+    );
+  }
+}
