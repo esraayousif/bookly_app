253b7580a2794af5c818c5357a802bd234b42845
codetime30 <ey989063@gmail.com>
2023-11-08 11:26:26 +0200

    Create Custom Search TextField

--- lib/Features/home/presentation/views/widgets/custom_appbar.dart
+++ lib/Features/home/presentation/views/widgets/custom_appbar.dart
@@ -1,7 +1,9 @@
 
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 
@@ -20,7 +22,9 @@ class CustomAppBar extends StatelessWidget {
           ),
           const Spacer(),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.KSearchView);
+            },
             icon: const Icon(FontAwesomeIcons.magnifyingGlass),
           ),
         ],
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,14 +1,20 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+
+import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-
-      ],
+    return const Padding(
+      padding: KPaddingHorizontal30,
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+        ],
+      ),
     );
   }
 }
