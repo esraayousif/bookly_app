db59e29babce035ca05bd5984875a25f76219f5d
codetime30 <ey989063@gmail.com>
2023-11-13 15:55:45 +0200

    Newest Books Cubit part1

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,10 @@
+part of 'newest_books_cubit.dart';
+
+abstract class NewestBooksState extends Equatable {
+  const NewestBooksState();
+}
+
+class NewestBooksInitial extends NewestBooksState {
+  @override
+  List<Object> get props => [];
+}
