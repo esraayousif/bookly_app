eade7212dc9ae7965123320bdcc8b848e738b484
codetime30 <ey989063@gmail.com>
2023-11-20 13:54:02 +0200

    Display Feature Books Part1

--- lib/Features/home/presentation/views/widgets/feature_list_view.dart
+++ lib/Features/home/presentation/views/widgets/feature_list_view.dart
@@ -1,6 +1,9 @@
-
-
+import 'package:bookly_app/Features/home/presentation/manager/features_books_cubit/features_books_cubit.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_image.dart';
 
@@ -9,16 +12,29 @@ class FeatherBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 8),
-              child: CustomBookImage(),
-            );
-          }),
+    return BlocBuilder<FeaturesBooksCubit, FeaturesBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height * .3,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomBookImage(),
+                  );
+                }),
+          );
+        }else if (state is FeaturedBooksFailure){
+          return CustomErrorWidget(errMessage: state.errMessage);
+        }else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class BooklyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => FeaturesBooksCubit(
               getIt.get<HomeRepoImpl>()
-          ),
+          )..fetchFeaturedBooks(),
         ),
         BlocProvider(
           create: (context) => NewestBooksCubit(
